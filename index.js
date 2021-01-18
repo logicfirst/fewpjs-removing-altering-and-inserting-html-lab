3d7165872ca650c0c0c40dba92188ddb99438054
logicfirst <slee@logicfirstllc.com>
2021-01-17 19:35:54 -0500

    commit tree 628a1ee0b53ed475c2a1f310da6dcb70241d1c79

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+function removeMain(){
+  let elmain = document.querySelector('#main')
+  elmain.remove()
+}
+
+removeMain()
+
+function newHead(){
+  let newHeader = document.createElement('h1')
+  newHeader.setAttribute('id', 'victory')
+  document.body.appendChild(newHeader)
+  newHeader.innerText = "Scott is the champion"
+}
+
+newHead()
